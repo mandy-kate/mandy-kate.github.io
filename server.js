8ebc7e30d1714defe79d2ad9411c5096d0169efd
mandy-kate <mandykatebaker@gmail.com>
2016-09-25 16:36:19 +1300

    simple server set up

---
+++ server.js
@@ -0,0 +1,18 @@
+var path = require('path')
+var express = require('express')
+
+
+var app = express()
+
+app.use(express.static(path.join(__dirname, './')))
+app.use(require('cookie-parser')())
+app.use(require('body-parser').urlencoded({ extended: true }))
+
+app.get('/', function (req, res) {
+  console.log("getting here")
+  res.sendFile('./index.html')
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('server is running on 3000')
+})
