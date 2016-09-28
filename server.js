334d3cb2eb1fac88d8f5924685fc1dc788c3f9cf
mandy-kate <mandykatebaker@gmail.com>
2016-09-28 22:18:40 +1300

    not bundling

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(express.static(path.join(__dirname, './')))
 app.use(require('cookie-parser')())
 app.use(require('body-parser').urlencoded({ extended: true }))
 
-// app.get('/', function (req, res) {
-//   console.log("getting here")
-//   res.sendFile('./index.html')
-// })
+app.get('/', function (req, res) {
+  console.log("getting here")
+  res.sendFile('./index.html')
+})
 
-app.get('*', function(req, res) {
-  res.sendFile(path.resolve(__dirname, 'public/index.html'));
-});
+// app.get('*', function(req, res) {
+//   res.sendFile(path.resolve(__dirname, 'public/index.html'));
+// });
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('server is running on 3000')
